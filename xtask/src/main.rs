c8006228cabdce66338b48d7207ca6835881b299
leegeunhyeok <dev.ghlee@gmail.com>
2025-10-16 05:58:55 +0900

    chore: release v0.1.0-beta.0

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -6,10 +6,12 @@ mod utils;
 
 fn main() -> Result<()> {
     let task = env::args().nth(1);
+    let opt = env::args().nth(2);
+
     match task.as_deref() {
         Some("version") => tasks::version::run(),
         Some("publish") => tasks::publish::run(),
-        Some("prepare") => tasks::prepare::run(),
+        Some("prepare") => tasks::prepare::run(opt),
         Some("build") => tasks::build::run(),
         _ => {
             eprintln!("Usage: cargo xtask [version|publish]");
--- xtask/src/tasks/prepare.rs
+++ xtask/src/tasks/prepare.rs
@@ -3,16 +3,25 @@ use craby_build::constants::toolchain::BUILD_TARGETS;
 
 use crate::utils::run_command;
 
-const EXCLUDE_PACKAGE_NAMES: [&str; 3] = ["craby-test", "craby-0.76", "craby-0.80"];
+pub const EXCLUDE_PACKAGE_NAMES: [&str; 3] = ["craby-test", "craby-0.76", "craby-0.80"];
 
-pub fn run() -> Result<()> {
-    println!("Preparing...");
+pub fn run(opt: Option<String>) -> Result<()> {
+    let is_ts = opt.is_some_and(|o| o == "--ts");
 
-    run_command("cargo", &["--version"], None)?;
+    println!(
+        "Preparing for {} integrations...",
+        if is_ts { "TypeScript" } else { "Rust" }
+    );
 
-    for target in BUILD_TARGETS {
-        println!("Installing target: {}", target.to_str());
-        run_command("rustup", &["target", "install", target.to_str()], None)?;
+    // Setup toolchain targets for Rust
+    if !is_ts {
+        println!("Setting up toolchain targets...");
+        run_command("cargo", &["--version"], None)?;
+
+        for target in BUILD_TARGETS {
+            println!("Installing target: {}", target.to_str());
+            run_command("rustup", &["target", "install", target.to_str()], None)?;
+        }
     }
 
     println!("Building packages...");
